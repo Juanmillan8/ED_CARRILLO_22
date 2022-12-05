71f7cfb829f1afe449bc6db6db9d6445dde778f4
Juanmillan8 <116264933+Juanmillan8@users.noreply.github.com>
2022-12-05 14:11:56 +0100

    Add files via upload

---
+++ UD02.4. Proyecto Maven - Eclipse/FibonacciMain/src/main/java/es/iescarrillo/daw/endes/FibonacciMain/fibonacciMain.java
@@ -0,0 +1,11 @@
+package es.iescarrillo.daw.endes.FibonacciMain;
+
+import es.iescarrillo.daw.endes.fibonacci.*;
+
+public class fibonacciMain 
+{
+    public static void main( String[] args )
+    {
+        Fibonacci.main(null);
+    }
+}
